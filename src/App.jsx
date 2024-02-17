4cae14caa24ea4d20bb19b8dc22c72c42fc19555
sandysan77 <santhoshsri84@gmail.com>
2024-02-16 22:19:22 -0500

    update

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+import Footer from './Footer';
+import Header from './Header';
+import Nav from './Nav';
+import Section1 from './Section1';
+import Section2 from './Section2';
+import Section3 from './Section3';
+import Section4 from './Section4';
+
+function App() {
+  
+
+  return (
+    <>
+    <Nav/>
+    <Header/>
+    <Section1/>
+    <Section2/>
+    <Section3/>
+    <Section4/>
+    <Footer/>
+    </> 
+  )
+}
+
+export default App
